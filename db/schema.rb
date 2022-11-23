0c8d7fa017967167fc07718c1fef77f136aae99c
Yllka Shala <yllkaaa.sha@gmail.com>
2022-11-23 12:56:55 +0100

    add address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_113530) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_115412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_113530) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
